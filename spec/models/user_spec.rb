8b98df8e465d1581fd5a9424edfaadde378edd5e
Lee Edwards <leeredwards@gmail.com>
2010-06-18 17:53:24 -0400

    Added devise models and views

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = User.new(:email => "darthvader@empire.com", :password => "iwantthemalive", :password_confirmation => "iwantthemalive")
+    user.should be_valid
+  end
+  
+  it "is not valid with no email address" do
+    user = User.new(:email => nil, :password => "iwantthemalive", :password_confirmation => "iwantthemalive")
+    user.should_not be_valid
+  end
+  
+  it "is not valid with a poorly formed email address" do
+    user = User.new(:email => "fake.whatisemail@wtf", :password => "iwantthemalive", :password_confirmation => "iwantthemalive")
+    user.should_not be_valid
+  end
+  
+  it "is not valid with no password" do
+    user = User.new(:email => "darthvader@empire.com", :password => nil, :password_confirmation => "iwantthemalive")
+    user.should_not be_valid
+  end
+  
+  it "is not valid with mismatched passwords" do
+      user = User.new(:email => "darthvader@empire.com", :password => "noooooooooooooooo", :password_confirmation => "iwantthemalive")
+      user.should_not be_valid
+    end  
 end
