daf0adaf919d84e882df2366335d3f6c18a5c1c5
Lee Edwards <leeredwards@gmail.com>
2010-11-18 20:43:02 -0500

    Removed Rails::Test folder and updated spec_helper.rb to speed up tests using Paperclip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,21 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  module Paperclip
+    class Geometry
+      def self.from_file file
+        parse("100x100")
+      end
+    end
+    class Thumbnail
+      def make
+        src = Test::FileHelper.fixture_file('white_pixel.jpg')
+        dst = Tempfile.new([@basename, @format].compact.join("."))
+        dst.binmode
+        FileUtils.cp(src.path, dst.path)
+        return dst
+      end
+    end
+  end
 end
