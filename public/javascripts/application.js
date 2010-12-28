d6cb2f584e7456137c03d9c355398c913269d9fe
Lee Edwards <leeredwards@gmail.com>
2010-12-27 19:52:06 -0500

    Added hint text for login, password, and search

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+	set_hint_text("#search_box", "Search for cards");
+	set_hint_text("#user_email", "Your email address");
+	set_hint_text("#user_password", "password");
+});
+
+function set_hint_text(selector, text){
+	$(selector).focus(function() {
+		$(this).val("");
+	});
+
+	$(selector).blur(function() {
+		if ($(this).val() == "") {
+			$(this).val(text);
+		};
+	});
+};
