f2b06fb8d6b7ee6aa9f5d19840678416ec263af1
Lee Edwards <leeredwards@gmail.com>
2013-05-04 21:35:21 -0400

    Add encryptable to keep devise on sha1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable, :activatable, :confirmable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :encryptable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
-  
+
   has_and_belongs_to_many :roles
   has_many :cardlists
-  
+
   def has_role?(role)
     return self.roles.include?(Role.find_by_name role)
   end
